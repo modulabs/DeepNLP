d43439e224185f964853dea3371665ad5320f0e0
Ryan S. Shin <sungjin7127@gmail.com>
2018-08-18 17:38:02 +0900

    quora update

--- Kaggle/QuoraQuestionPairs/quora_malstm_ryan.py
+++ Kaggle/QuoraQuestionPairs/quora_malstm_ryan.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime 
 import json
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -18,6 +18,8 @@ from tensorflow.python.keras import backend as K
 # from tensorflow.python.keras._impl.keras.layers import Embedding
 # from tensorflow.python.keras._impl.keras.layers import Reshape, Flatten, Dropout, Concatenate, dot, add
 
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
+
 ## 미리 Global 변수를 지정하자. 파일 명, 파일 위치, 디렉토리 등이 있다.
 
 Q1_TRAINING_DATA_FILE = 'q1_train.npy'
@@ -29,12 +31,12 @@ DATA_PATH = './data/'
 
 ## 학습에 필요한 파라메터들에 대해서 지정하는 부분이다.
 
-BATCH_SIZE = 1024
+BATCH_SIZE = 256
 EPOCH = 50
 HIDDEN = 50
 BUFFER_SIZE = 2048
 
-TEST_SPLIT = 0.1
+TEST_SPLIT = 0.2
 RNG_SEED = 13371447
 EMBEDDING_DIM = 50
 MAX_SEQ_LEN = 25
@@ -56,8 +58,8 @@ with open(DATA_PATH+NB_WORDS_DATA_FILE, 'r') as f:
 X = np.stack((q1_data, q2_data), axis=1) 
 y = labels
 
-# X = word_embedding_matrix[X[:50000]]
-# y = y[:50000]
+# X = word_embedding_matrix[X[:1000]]
+# y = y[:1000]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SPLIT, random_state=RNG_SEED)
 Q1_train = X_train[:,0]
@@ -72,60 +74,48 @@ def rearrange(q, sim_q, labels):
 def train_input_fn():
     dataset = tf.data.Dataset.from_tensor_slices((Q1_train, Q2_train, y_train))
     dataset = dataset.shuffle(buffer_size=len(Q1_train))
-    dataset = dataset.batch(16)
+    dataset = dataset.batch(BATCH_SIZE)
     dataset = dataset.map(rearrange)
     dataset = dataset.repeat()
     iterator = dataset.make_one_shot_iterator()
     
     return iterator.get_next()
 
-def test_input_fn():
+def eval_input_fn():
     dataset = tf.data.Dataset.from_tensor_slices((Q1_test, Q2_test, y_test))
     dataset = dataset.shuffle(buffer_size=len(Q1_test))
-    dataset = dataset.batch(16)
+    dataset = dataset.batch(BATCH_SIZE)
     dataset = dataset.map(rearrange)
     dataset = dataset.repeat()
     iterator = dataset.make_one_shot_iterator()
     
     return iterator.get_next()
 
-def RNN_attention(features, labels, mode):
+def Malstm(features, labels, mode):
     
     TRAIN = mode == tf.estimator.ModeKeys.TRAIN
     EVAL = mode == tf.estimator.ModeKeys.EVAL
     PREDICT = mode == tf.estimator.ModeKeys.PREDICT
     
-    labels = tf.reshape(labels, [-1,1])
-
-    # Embedding layer
-    # with tf.device('/cpu:0'), tf.name_scope("embedding"):
-    #     W = tf.Variable(
-    #         tf.random_uniform([nb_words+1, EMBEDDING_DIM], -1.0, 1.0),
-    #         name="W")
-    #     q1_emb = tf.nn.embedding_lookup(W, features['q'])
-    #     q1_emb_expand = tf.expand_dims(q1_emb, -1)
-
-    #     q2_emb = tf.nn.embedding_lookup(W, features['sim_q'])
-    #     q2_emb_expand = tf.expand_dims(q2_emb, -1)
-
-    q1 = layers.Embedding(nb_words + 1, 
-                 EMBEDDING_DIM, 
-                 weights=[word_embedding_matrix], 
-                 input_length=MAX_SEQ_LEN, 
-                 trainable=False)(features['q'])
+    # labels = tf.to_float(tf.reshape(labels, [-1,1]))
     
-    q2 = layers.Embedding(nb_words + 1, 
-                 EMBEDDING_DIM, 
-                 weights=[word_embedding_matrix], 
-                 input_length=MAX_SEQ_LEN, 
-                 trainable=False)(features['sim_q'])
+    with tf.device('/cpu:0'), tf.name_scope("embedding"):
+        glove_weights_initializer = tf.constant_initializer(word_embedding_matrix)
+        embedding_weights = tf.get_variable(
+            name='embedding_weights',
+            initializer = glove_weights_initializer,
+            shape=(nb_words+1, EMBEDDING_DIM), 
+            trainable=False)
 
+        q1 = tf.nn.embedding_lookup(embedding_weights, features['q'])
+        q2 = tf.nn.embedding_lookup(embedding_weights, features['sim_q'])
+            
     with tf.variable_scope('q_bi_lstm'):
         lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(num_units = 64, activation = tf.nn.tanh)
         lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(num_units = 64, activation = tf.nn.tanh)
         _, output_states = tf.nn.bidirectional_dynamic_rnn(cell_fw = lstm_fw_cell,
                                                           cell_bw = lstm_bw_cell,
-                                                          inputs = q1_emb_expand,
+                                                          inputs = q1,
                                                           dtype = tf.float32)
 
         q_final_state = tf.concat([output_states[0].h, output_states[1].h], axis=1)
@@ -136,7 +126,7 @@ def RNN_attention(features, labels, mode):
         lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(num_units = 64, activation = tf.nn.tanh)
         _, output_states = tf.nn.bidirectional_dynamic_rnn(cell_fw = lstm_fw_cell,
                                                           cell_bw = lstm_bw_cell,
-                                                          inputs = q2_emb_expand,
+                                                          inputs = q2,
                                                           dtype = tf.float32)
         
         sim_final_state = tf.concat([output_states[0].h, output_states[1].h], axis=1)
@@ -144,34 +134,35 @@ def RNN_attention(features, labels, mode):
     with tf.variable_scope('output_layer'):
         #Malstm
         output = K.exp(-K.sum(K.abs(q_final_state -  sim_final_state), axis=1, keepdims=True))
-                
-    if TRAIN:
-            
-        global_step = tf.train.get_global_step()
         
-        loss = tf.losses.mean_squared_error(labels, output)
-#         loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.one_hot(labels, depth=2),
-#                                                logits = output
-#                                               )
-#         tf.summary.scalar('loss', loss)
+                
+    if TRAIN:            
+        global_step = tf.train.get_global_step()
+        output = tf.squeeze(output, 1)
 
-        train_op = tf.train.AdamOptimizer(1e-4).minimize(loss, global_step)        
+        loss =tf.losses.mean_squared_error(labels=labels, predictions=output)
+
+        tf.summary.scalar('loss', loss)
+
+        train_op = tf.train.AdamOptimizer(1e-4).minimize(loss, global_step)
 
         return tf.estimator.EstimatorSpec(mode=mode, train_op=train_op, loss = loss)
     
     elif EVAL:
-        loss = tf.losses.mean_squared_error(labels, output)
-        pred = tf.nn.sigmoid(output)
-        accuracy = tf.metrics.accuracy(labels, tf.round(pred))
+        output = tf.squeeze(output, 1)
+        loss =tf.losses.mean_squared_error(labels=labels, predictions=output)
+        accuracy = tf.metrics.accuracy(labels, tf.round(output))
+
+        tf.summary.scalar('loss', loss)
+        tf.summary.scalar('accuracy', accuracy)
+
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops={'acc': accuracy})
         
     elif PREDICT:
         return tf.estimator.EstimatorSpec(
             mode=mode,
             predictions={
-                'prob': cos_sim,
-                'q_sem': q_dense_layer,
-                'sim_q_sem': sim_q_dense_layer
+                'prob': output
             }
         )
 
@@ -182,7 +173,7 @@ config_tf = tf.estimator.RunConfig()
 config_tf._save_checkpoints_steps = 100
 config_tf._save_checkpoints_secs = None
 config_tf._keep_checkpoint_max =  2
-config_tf._log_step_count_steps = 100
+config_tf._log_step_count_steps = 10
 
 time_start = datetime.now()
 
@@ -194,11 +185,12 @@ print(".......................................")
 tf.logging.set_verbosity(tf.logging.INFO)
 print(tf.__version__)
 
-train_spec = tf.estimator.TrainSpec(train_input_fn, max_steps=10000)
-# eval_spec = tf.estimator.EvalSpec(eval_input_fn, steps=100)
+dssm_est = tf.estimator.Estimator(Malstm, model_dir=model_dir, config=config_tf)
 
-dssm_est = tf.estimator.Estimator(RNN_attention, model_dir=model_dir, config=config_tf)
-dssm_est.train(train_input_fn, steps=10000)
+train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=10200)
+eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
+
+tf.estimator.train_and_evaluate(dssm_est, train_spec, eval_spec)
 
 time_end = datetime.now()
 
@@ -207,3 +199,9 @@ print("Experiment finished at {}".format(time_end.strftime("%H:%M:%S")))
 print("")
 time_elapsed = time_end - time_start
 print("Experiment elapsed time: {} seconds".format(time_elapsed.total_seconds()))
+
+# For prediction or extract values
+# prediction = dssm_est.predict(test_input_fn)
+#
+# for i, p in enumerate(prediction):
+#     print(i, p['prob'])
