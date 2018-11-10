53a5d99a1db05064623cabab85aec18ab940ee8b
Jisoo <jskim@mindmap.ai>
2018-11-10 12:05:12 +0900

    added taekyoons transformer

---
+++ 7.NLPBOOK/6.CHATBOT/ChatBot_Transformer_by_TK/configs.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+import tensorflow as tf
+
+tf.app.flags.DEFINE_integer('batchSize', 40, 'batch size') # 배치 크기
+tf.app.flags.DEFINE_integer('trainSteps', 10000, 'train steps') # 학습 에포크
+tf.app.flags.DEFINE_float('dropoutWidth', 0.8, 'dropout width') # 드롭아웃 크기
+tf.app.flags.DEFINE_integer('layerSize', 1, 'layer size') # 멀티 레이어 크기 (multi rnn)
+tf.app.flags.DEFINE_integer('hiddenSize', 128, 'weights size') # 가중치 크기
+tf.app.flags.DEFINE_float('learningRate', 1e-3, 'learning rate') # 학습률
+tf.app.flags.DEFINE_string('dataPath', './data/ChatBotData.csv', 'data path') #  데이터 위치
+tf.app.flags.DEFINE_string('vocabularyPath', './data/vocabularyData.voc', 'vocabulary path') # 사전 위치
+tf.app.flags.DEFINE_string('checkPointPath', './checkPoint', 'check point path') # 체크 포인트 위치
+tf.app.flags.DEFINE_integer('shuffleSeek', 1000, 'shuffle random seek') # 셔플 시드값
+tf.app.flags.DEFINE_integer('maxSequenceLength', 25, 'max sequence length') # 시퀀스 길이
+tf.app.flags.DEFINE_integer('embeddingSize', 128, 'embedding size') # 임베딩 크기
+tf.app.flags.DEFINE_boolean('tokenizeAsMorph', False, 'set morph tokenize') # 형태소에 따른 토크나이징 사용 유무
+tf.app.flags.DEFINE_boolean('embedding', True, 'Use Embedding flag') # 임베딩 유무 설정
+tf.app.flags.DEFINE_boolean('multilayer', True, 'Use Multi RNN Cell') # 멀티 RNN 유무
+tf.app.flags.DEFINE_string('saveModelPath', './model', 'save model')
+# Define FLAGS
+DEFINES = tf.app.flags.FLAGS
