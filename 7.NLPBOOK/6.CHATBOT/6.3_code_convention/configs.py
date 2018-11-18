609ad95c423a73c0804f695953e1248cf0f508a5
changwookjun <changwookjun@gmail.com>
2018-11-18 15:31:45 +0900

    modify median value

--- 7.NLPBOOK/6.CHATBOT/6.3_code_convention/configs.py
+++ 7.NLPBOOK/6.CHATBOT/6.3_code_convention/configs.py
@@ -7,7 +7,7 @@ tf.app.flags.DEFINE_float('dropout_width', 0.5, 'dropout width') # 드롭아웃 
 tf.app.flags.DEFINE_integer('layer_size', 3, 'layer size') # 멀티 레이어 크기 (multi rnn)
 tf.app.flags.DEFINE_integer('hidden_size', 128, 'weights size') # 가중치 크기
 tf.app.flags.DEFINE_float('learning_rate', 1e-3, 'learning rate') # 학습률
-tf.app.flags.DEFINE_string('data_path', './../data_in/ChatBotData.csv', 'data path') #  데이터 위치
+tf.app.flags.DEFINE_string('data_path', './data_in/ChatBotData.csv', 'data path') #  데이터 위치
 tf.app.flags.DEFINE_string('vocabulary_path', './data_out/vocabularyData.voc', 'vocabulary path') # 사전 위치
 tf.app.flags.DEFINE_string('check_point_path', './data_out/check_point', 'check point path') # 체크 포인트 위치
 tf.app.flags.DEFINE_integer('shuffle_seek', 1000, 'shuffle random seek') # 셔플 시드값
